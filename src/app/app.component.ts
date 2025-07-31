62ce2b1f521e045d8cec26adbb03e679dd3a8bcd
jeanitaz <jeanzeykoharcode@gmail.com>
2025-07-31 12:47:29 -0500

    Prueba de navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { ProductosComponent } from "./components/productos/productos.component";
+import { NavbarComponent } from "./components/navbar/navbar.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [ProductosComponent],
+  imports: [ProductosComponent, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
