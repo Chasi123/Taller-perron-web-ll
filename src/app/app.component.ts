0c248829e77c201d923563c570b4aa07a4656712
jeanitaz <jeanzeykoharcode@gmail.com>
2025-07-31 12:40:43 -0500

    Primeros Inicios del Producto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { ProductosComponent } from "./components/productos/productos.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [ProductosComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
