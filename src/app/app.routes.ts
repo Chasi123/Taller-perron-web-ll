c459b29918a9cda857e9af5feeaedaaf07dc5ad1
jeanitaz <jeanzeykoharcode@gmail.com>
2025-08-01 07:42:18 -0500

    formulario

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { HomeComponent } from './pages/home/home.component';
 import { ProductosComponent } from './components/productos/productos.component';
 import { NosotrosComponent } from './components/nosotros/nosotros.component';
 import { ContactoComponent } from './components/contacto/contacto.component';
+import { FormularioSuscriComponent } from './components/formulario-suscri/formulario-suscri.component';
 
 export const routes: Routes = [
     {path:"", component : HomeComponent},
     {path:"productos", component : ProductosComponent},
     {path:"sobre-nosotros", component : NosotrosComponent},
     {path:"contacto", component : ContactoComponent},
+    {path:"formulario", component : FormularioSuscriComponent},
     {path:"**", redirectTo: ""}
 ];
